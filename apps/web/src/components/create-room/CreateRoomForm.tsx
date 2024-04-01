0d0c6f619c603b05a38e31045fd2a6146b295a42
Avinash <avinash.kumar@renegadeinsurance.com>
2024-04-02 01:08:23 +0530

    feat(web): created room skeleton

--- apps/web/src/components/create-room/CreateRoomForm.tsx
+++ apps/web/src/components/create-room/CreateRoomForm.tsx
@@ -4,12 +4,17 @@ import { Box } from "@repo/ui";
 import { FlexCol } from "./style";
 
 export const CreateRoomForm = () => {
+  const handleSubmit = (data) => {
+    debugger;
+  };
   return (
-    <FlexCol>
-      <Box>Create new Room</Box>
-      <input placeholder="room name" />
-      <input placeholder="owner name" />
-      <button>Create</button>
-    </FlexCol>
+    <form onSubmit={handleSubmit}>
+      <FlexCol>
+        <Box>Create new Room</Box>
+        <input placeholder="room name" />
+        <input placeholder="owner name" />
+        <button type="submit">Create</button>
+      </FlexCol>
+    </form>
   );
 };
