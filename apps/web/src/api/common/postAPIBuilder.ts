6487d2585338f0159c840a1bd6c7aa3be0a2249f
Avinash <avinash.kumar@renegadeinsurance.com>
2024-04-17 01:36:42 +0530

    feat(web): added genric type for response

--- apps/web/src/api/common/postAPIBuilder.ts
+++ apps/web/src/api/common/postAPIBuilder.ts
@@ -1,9 +1,9 @@
 import { APIBuilder } from "./baseAPIBuilder";
 
 export class PostAPIBuilder extends APIBuilder {
-  sendRequest() {
+  sendRequest<TResponse>() {
     this.withHeader("Content-Type", "application/json");
     // this.withHeader("Content-Type", "application/x-www-form-urlencoded");
-    return this.sendRequestAndVerifyResponse("POST");
+    return this.sendRequestAndVerifyResponse<TResponse>("POST");
   }
 }
